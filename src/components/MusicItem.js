228dfb2a202bdcc9d619d7347579b4a436ddbbaa
Katherine Kerr <katherinekerr@protonmail.com>
2019-08-13 20:18:24 +0100

    adds album images to music item

--- src/components/MusicItem.js
+++ src/components/MusicItem.js
@@ -4,8 +4,13 @@ import './MusicItem.css';
 const MusicItem = (props) => (
   <div className="music-item">
     <li>
-      <p className="music-item-title"> {props.position} - {props.song['im:name'].label}</p>
-      <p> {props.song['im:artist'].label} </p>
+      <div>
+        <img src={props.song['im:image'][1].label}/>
+      </div>
+      <div>
+        <p className="music-item-title"> {props.position} - {props.song['im:name'].label}</p>
+        <p> {props.song['im:artist'].label} </p>
+      </div>
     </li>
   </div>
 )
